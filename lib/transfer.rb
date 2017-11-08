367999fd0d2272933216eccdf8263c8eda19d604
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-08 03:41:40 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,15 @@
 class Transfer
-  # your code here
+  attr_accessor :status
+  attr_reader :sender, :receiver, :amt
+
+  def initialize(sender, receiver, amt)
+    @sender = from
+    @receiver= to
+    @amt = amt
+    @status = "pending"
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
 end
