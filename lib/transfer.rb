d1821fe7b935cac18185b06f4199c87af5bcfb95
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-08 03:55:39 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,10 +14,10 @@ class Transfer
   end
 
   def execute_transaction
-    if self.valid? && @status == "pending"
+    if @sender.valid? && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
-      @status = "complet"
+      @status = "complete"
     else
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
