455ff62159b8be0b01fea2de5dd9cc64e2a35ab0
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-08 03:35:43 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,27 @@
 class BankAccount
+  attr_accessor :balance, :status
+  attr_reader :name
 
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def display_balance
+    @balance
+  end
+
+  def deposit(amt)
+    self.display_balance += amt
+  end
+
+  def valid?
+    @status == "open" && @balance > 0 ? true : false
+  end
+
+  def close_account
+    @status = "closed"
+  end
 end
+
