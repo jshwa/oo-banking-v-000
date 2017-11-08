7de355dc259645ffbf6864db6708dc74c5c3f196
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-08 03:57:07 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.valid? && @status == "pending" && false
+    if @sender.valid? && @status == "p"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
